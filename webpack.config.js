68b0d10a46b9d586a069e6a2e59d756131e99a8a
Pavel Pochobradský <Pochobradsky@porg.cz>
2018-10-05 13:17:02 +0200

    File structure and building

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+
+var path = require("path");
+var webpack = require("webpack");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var version = require('./package.json').version;
+
+module.exports = {
+  entry: "./src/index.jsx",
+  node: {
+    fs: "empty"
+  },
+  output: {
+    filename: "bundle" + version + ".js",
+    chunkFilename: "[name].bundle" + version + ".js",
+    publicPath: "/Scripts/",
+    path: path.join( __dirname, "out/scripts")
+  },
+  mode: 'production',
+  plugins: [
+		new HtmlWebpackPlugin({
+      title: 'My App',
+      template: './src/index.html',
+      filename: "../index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style" + version + ".css",
+      chunkFilename: "something.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /utils/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+      {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      }
+    ]
+  }
+};
